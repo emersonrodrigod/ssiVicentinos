0bd878231ec3851531f488e95d31b1679f44c45c
ti <ti@ti-PC>
2013-10-23 10:22:14 -0200

    Adicionado o Script ssi.sql com o schema do banco de dados na pasta DOCS do projeto.

---
+++ docs/ssi.sql
@@ -0,0 +1,183 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Máquina: localhost
+-- Data de Criação: 23-Out-2013 às 12:20
+-- Versão do servidor: 5.6.12-log
+-- versão do PHP: 5.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `ssi`
+--
+CREATE DATABASE IF NOT EXISTS `ssi` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ssi`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `departamento`
+--
+
+CREATE TABLE IF NOT EXISTS `departamento` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(155) NOT NULL,
+  `ativo` int(11) NOT NULL DEFAULT '1',
+  `id_empresa` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_departamento_empresa_idx` (`id_empresa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `empresa`
+--
+
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `ativo` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `historico_ssi`
+--
+
+CREATE TABLE IF NOT EXISTS `historico_ssi` (
+  `id` int(11) NOT NULL,
+  `dataHora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `descricao` varchar(255) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_ssi` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_historico_usuario_idx` (`id_usuario`),
+  KEY `fk_historico_ssi_idx` (`id_ssi`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `log_acessos`
+--
+
+CREATE TABLE IF NOT EXISTS `log_acessos` (
+  `id` int(11) NOT NULL,
+  `dataEntrada` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `dataSaida` timestamp NULL DEFAULT NULL,
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_log_acessos_usuario_idx` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ssi`
+--
+
+CREATE TABLE IF NOT EXISTS `ssi` (
+  `id` int(11) NOT NULL,
+  `dataAbertura` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `resumo` varchar(200) NOT NULL,
+  `descricao` text NOT NULL,
+  `previsaoEncerramento` timestamp NULL DEFAULT NULL,
+  `status` varchar(15) NOT NULL DEFAULT 'PENDENTE',
+  `dataEncerramento` timestamp NULL DEFAULT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_responsavel` int(11) DEFAULT NULL,
+  `id_tipo` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_ssi_usuario_abertura_idx` (`id_usuario`),
+  KEY `fk_ssi_usuario_responsável_idx` (`id_responsavel`),
+  KEY `fk_ssi_tipo_ssi_idx` (`id_tipo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipo_ssi`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo_ssi` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `ativo` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `ativo` int(11) NOT NULL DEFAULT '1',
+  `dataCadastro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` varchar(45) NOT NULL,
+  `id_empresa` int(11) DEFAULT NULL,
+  `id_departamento` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_usuario_empresa_idx` (`id_empresa`),
+  KEY `fk_usuario_departamento_idx` (`id_departamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `departamento`
+--
+ALTER TABLE `departamento`
+  ADD CONSTRAINT `fk_departamento_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `historico_ssi`
+--
+ALTER TABLE `historico_ssi`
+  ADD CONSTRAINT `fk_historico_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_historico_ssi` FOREIGN KEY (`id_ssi`) REFERENCES `ssi` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `log_acessos`
+--
+ALTER TABLE `log_acessos`
+  ADD CONSTRAINT `fk_log_acessos_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `ssi`
+--
+ALTER TABLE `ssi`
+  ADD CONSTRAINT `fk_ssi_usuario_abertura` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ssi_usuario_responsável` FOREIGN KEY (`id_responsavel`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ssi_tipo_ssi` FOREIGN KEY (`id_tipo`) REFERENCES `tipo_ssi` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limitadores para a tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `fk_usuario_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_usuario_departamento` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
